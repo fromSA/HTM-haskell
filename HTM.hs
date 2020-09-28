a057dea51dbea519ec870bc389c41405a6b3d512
Fromsa Hera <fromsahera28@gmail.com>
2020-09-28 11:58:12 +0200

    implement the datastructure, the overlap of columns with input, add configs

--- HTM.hs
+++ HTM.hs
@@ -1,8 +1,7 @@
 module HTM (spacialPooler, temporalPooler) where
 
-import           Data.List     (intercalate)
+import           Data.List     (intercalate, sortOn)
 import           System.Random
-
 {-
 
   MODEL
@@ -18,6 +17,9 @@ data Config = Config {
   , mappingType            :: MappingType
   , initConnectionStrength :: Float -- on the synapses between cells in a region
   , sdrRange               :: SDRRange
+  , overlapThreshold       :: Int -- between inputField and column
+  , pConthresh             :: Float -- permanence connected threshold on a synapse
+  , colActLev              :: Int -- the desired column activity level within inhibition radius
 }
 
 
@@ -55,10 +57,10 @@ totNrBits config = sum (map ($ config) [buckets, bitsPerBucket]) - 1
 -- TODO take an input and convert it to an SDR
 encode :: Int -> SDRConfig -> SDR
 encode n config = let start = getStartOf n config in
-  [start + i | i <- [0..bitsPerBucket config]]
+  [start + i | i <- [0..(bitsPerBucket config - 1)]]
 
 getStartOf :: Int -> SDRConfig -> Int
-getStartOf n config = floor $ realToFrac (n - (minVal config)) / (realToFrac ((maxVal config) - (minVal config)))
+getStartOf n config = floor $  realToFrac ((buckets config) * (n - (minVal config))) / (realToFrac ((maxVal config) - (minVal config)))
 
 
 -- Mapper between SDR and Region
@@ -72,22 +74,29 @@ data Region = Region {
 }
 
 instance Show Region where
-  show region = show $ currentStep region
+  show = show . currentStep 
 
 data Column = Column {
   cells         :: [Cell]
   , inputField  :: [FeedForwardSynapse]
   , howActive   :: MovingAverage -- TODO the average rate of activation
   , columnState :: ColumnState
+  , boost       :: Float -- should maybe be Float, should be at least 1
+  , overlap     :: Int
+  , inhibRad    :: Int
 }
 
+instance Eq Column where
+  c1 == c2 = (inputField c1) == (inputField c2)
+
 instance Show Column where
-  show column = intercalate "" $ map show $ cells column
+  show  = show . overlap 
+  --show column = intercalate "" $ map show $ cells column
 
 type FeedForwardSynapse = (BitIndex, ConnectionStrength)
 
 instance Show Cell where
-  show (cell) = show $ cellState cell
+  show = show . cellState 
 
 data Cell = Cell {
   dendrites   :: [Dendrite]
@@ -97,13 +106,16 @@ data Cell = Cell {
 
 
 data ColumnState = ActiveColumn | InactiveColumn deriving (Eq)
+instance Show ColumnState where
+  show ActiveColumn = "1"
+  show InactiveColumn = "0"
+
 data CellState = ActiveCell | InactiveCell | PredictiveCell deriving (Eq)
 
 instance Show CellState where
-  show state
-    | state == ActiveCell = "1"
-    | state == InactiveCell = "0"
-    | state == PredictiveCell = "p"
+  show ActiveCell = "1"
+  show InactiveCell = "0"
+  show PredictiveCell = "p"
 
 type Dendrite = [Segment]
 type Segment = [Synapse]
@@ -157,6 +169,9 @@ singleColumn columnIndex config = Column {
   , inputField = initFeedForwardSynapses columnIndex config
   , howActive = (MovingAverage []) -- TODO the average rate of activation
   , columnState = InactiveColumn
+  , boost       = 1 -- should maybe be Float
+  , overlap     = 0
+  , inhibRad    = 0
 }
 
 initCells :: Config -> [Cell]
@@ -214,7 +229,7 @@ getRandomCell config notCell columns = let randColumnIndex = getRandomIndexBetwe
 
 -- init mapping between sdr indecies and Columns in the region
 initFeedForwardSynapses :: ColumnIndex -> Config -> [FeedForwardSynapse]
-initFeedForwardSynapses cI con = [singleFeedForwardSynapse bI con | bI <- selectRandomIndecies cI con]
+initFeedForwardSynapses cI con = [singleFeedForwardSynapse bI con | bI <- selectRandomIndecies cI con] -- FIXME this is a list of the synapses 
 
 selectRandomIndecies :: ColumnIndex -> Config -> [BitIndex]
 selectRandomIndecies cI con = randIndecies (maxNrOfInputBits con) cI (sdrRange con)
@@ -222,7 +237,7 @@ selectRandomIndecies cI con = randIndecies (maxNrOfInputBits con) cI (sdrRange c
 randIndecies :: Int -> ColumnIndex -> SDRRange -> [BitIndex]
 randIndecies n cI sR
   | n <= 0 = []
-  | n > 0 = randIndex cI sR : randIndecies (n-1) cI sR --TODO double check that no duplicates occure
+  | n > 0 = randIndex cI sR : randIndecies (n-1) cI sR -- TODO double check that no duplicates occure
 
 getRandomIndexBetween :: Int -> Int -> Int -> BitIndex
 getRandomIndexBetween seed mi ma = let g = mkStdGen seed in
@@ -241,22 +256,97 @@ singleFeedForwardSynapse index config = (index, initConnectionStrength config) -
 
 -}
 
+
+{-
+  SpatialPooler
+-}
+
+-- TODO applie a spacialPooler on a region
+spacialPooler :: Config -> SDR -> Region -> Region
+--spacialPooler c i r = updatePermanence . updateBoost $ activateColumns c i r
+spacialPooler c i r = activateColumns c i r
+
+-- initialize
+    -- first map input to region -- DONE
+-- phase1 : overlap
+    -- activate columns based on their input field
+-- phase2 : Inhibition
+    -- Boost the column (to maintain a fixed sparcity)
+-- phase 3 : learning
+    -- update permenance values of each Sensory
+
+
+activateColumns :: Config -> SDR -> Region -> Region
+activateColumns config sdr region = region { currentStep = updateOverlap config sdr (currentStep region)}
+--activateColumns config sdr region = region { currentStep = learn . (updateInhibition config) $ updateOverlap config sdr (currentStep region)}
+-- for each column -> compute overlap & boost it by the boost factor
+-- for each column -> activate columns if the column is not inhibited
+-- Learning: for each active column -> update permanence
+-- UpdateBoosting: for each columns ->
+  -- update boosting/
+  -- update active duty & overlap cycle
+  -- update inhibition radius
+
+
+-- Overlap
+updateOverlap :: Config -> SDR -> [Column] -> [Column]
+updateOverlap config sdr columns = map (computeOverlap config sdr) columns
+
+computeOverlap :: Config -> SDR -> Column -> Column
+computeOverlap config sdr column = let overlap = countOverlap config sdr (inputField column) in
+  if overlap >= (overlapThreshold config) then
+    column{overlap = floor $ fromIntegral(overlap) * (boost column)}
+  else column{overlap = 0 }
+
+
+countOverlap :: Config -> SDR -> [FeedForwardSynapse] -> Int
+countOverlap config sdr = sum . map (choose config sdr) 
+
+-- |The `choose` function chooses if a column is active or not based on
+-- the connection strength and activation threshold
+choose :: Config -> SDR -> FeedForwardSynapse -> Int
+choose config sdr (bitIndex, connectionStrength)
+  | connectionStrength >= (pConthresh config) = if elem bitIndex sdr then 1 else 0
+  | otherwise = 0
+
+
+
+--Inhibition
+updateInhibition :: Config -> [Column] -> [Column]
+updateInhibition config columns = [maybeActivateColumn config (col,ind) columns | (col,ind) <- zip columns [1..] ]
+
+maybeActivateColumn :: Config -> (Column,Int) -> [Column] -> Column
+maybeActivateColumn config (col,ind) cols =
+  (maybeActivate col) . (kmaxOverlap (colActLev config)) $ neighbors (inhibRad col) ind cols
+
+neighbors :: Int -> Int -> [Column] -> [Column]
+neighbors inhibRad colIndex cols = drop (colIndex - (inhibRad `div` 2)) . take (inhibRad) $ cols --TODO double check
+
+kmaxOverlap :: Int -> [Column] -> [Column]
+kmaxOverlap k cols = take k $ sortOn overlap cols -- TODO double check
+
+maybeActivate :: Column -> [Column] -> Column
+maybeActivate col cols
+  | col `elem` cols = col{columnState = ActiveColumn}
+  | otherwise = col{columnState = InactiveColumn}
+
+
+-- learn
+learn :: [Column] -> [Column]
+learn col = col -- TODO
+
+
+
 updatePermanence :: Region -> Region
 updatePermanence r = r -- TODO
 
-boost :: Region -> Region
-boost r = r -- TODO
+updateBoost :: Region -> Region
+updateBoost r = r -- TODO
 
-activateColumns :: SDR-> Region -> Region
-activateColumns i r = r -- TODO
 
--- TODO applie a spacialPooler on a region
-spacialPooler :: SDR-> Region -> Region
-spacialPooler i r = updatePermanence . boost . activateColumns i $ r
--- first map input to region -- DONE
--- second activate columns based on their input region
--- Boost the column (to maintain a fixed sparcity)
--- update permenance values of each Sensory
+{-
+  Temporal pooler
+-}
 
 
 -- TODO applie a spacialPooler on a region
@@ -286,25 +376,34 @@ temporalPooler m = m
 
 main = do
   let sdrConfig = initSDRConfig
-  let config = initConfig initSDRConfig
+  let config = initConfig sdrConfig
   let region = initRegion config
+  let encodedSDR = encode 50 sdrConfig
+  print encodedSDR
   print region
+  let region2 = spacialPooler config encodedSDR region
+  print region2
+
+-- main = print <$> spacialPooler <*> (encode 12 sdrConfig) <*> (initRegion initConfig initSDRConfig)
 
 initConfig :: SDRConfig -> Config
 initConfig sdrConfig = Config{
    nrOfColumns              = 100
    , nrOfCellsPerColumn     = 1
-   , maxNrOfInputBits       = 1
+   , maxNrOfInputBits       = 2 -- The nr of synapses from the inputregion to a column
    , nrOfSynapsesPerCell    = 1
    , mappingType            = Random
    , initConnectionStrength = 1.0 -- on the synapses between cells in a region, should be a kernel function
    , sdrRange               = SDRRange {minIndex  = 0, maxIndex = totNrBits sdrConfig}
+   , overlapThreshold = 0
+   , pConthresh = 0.7
+   , colActLev = 2
 }
 
 initSDRConfig :: SDRConfig
 initSDRConfig = SDRConfig{
   minVal          = 0
   , maxVal        = 100
-  , buckets       = 100
-  , bitsPerBucket = 5
+  , buckets       = 50
+  , bitsPerBucket = 50
 }
